dfc43ba7d2155e4fcb9bbf65efdf0dc3d64bec58
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-26 05:59:29 +0300

    headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export function GET() {
-  return new Response("Profile API data");
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // const token = requestHeaders.get("authorization");
+
+  const header = await headers();
+  const token = header.get("authorization");
+  console.log(`token:  ${token}`);
+
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {"Content-Type": "text/html"}
+  });
 }
