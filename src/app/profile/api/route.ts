a906a7a38f3ac21a6ec80f597886cca948d2fb62
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-26 09:08:48 +0300

    cookies in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -9,7 +9,14 @@ export async function GET(request: NextRequest) {
   const token = header.get("authorization");
   console.log(`token:  ${token}`);
 
+  console.log(request.cookies.get("theme"));
+
+  //! build-in cookies function
+  const cookieStore = await cookies();
+  cookieStore.set("resultPerPage", "20");
+  console.log(`result per page is: ${cookieStore.get("resultPerPage")?.value}`);
+
   return new Response("<h1>Profile API data</h1>", {
-    headers: {"Content-Type": "text/html"}
+    headers: { "Content-Type": "text/html", "Set-Cookie": "theme=dark" },
   });
 }
